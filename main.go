eeb3bf67c0f9184134bfc3e61a1bc14ae2928c99
Heather Frantisak <heather.frantisak@shopify.com>
2021-07-22 10:37:02 -0700

    Implement GameState

--- main.go
+++ main.go
@@ -50,37 +50,51 @@ func min(a, b int) int {
 	return b
 }
 
-func main()  {
+func Shuffle(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Deck = deck.New(deck.Deck(3), deck.Shuffle)
+	return ret
+}
+
+func Deal(gs GameState) GameState {
+	ret := clone(gs)
+	ret.Player = make(Hand, 0, 5)
+	ret.Dealer = make(Hand, 0, 5)
 	var card deck.Card
-	var player, dealer Hand
 	for i := 0; i < 2; i++ {
-		for _, hand := range []*Hand{&player, &dealer} {
-			card, cards = cards[0], cards[1:]
-			*hand = append(*hand, card)
-		}
+		card, ret.Deck = draw(ret.Deck)
+		ret.Player = append(ret.Player, card)
+		card, ret.Deck = draw(ret.Deck)
+		ret.Dealer = append(ret.Dealer, card)
 	}
-	var input string
-	for input != "s" {
-		fmt.Println("Player:", player)
-		fmt.Println("Dealer:", dealer.DealerString())
-		fmt.Println("What will you do? (h)it, (s)tand")
-		fmt.Scanf("%s\n", &input)
-		switch input {
-		case "h":
-			card, cards = draw(cards)
-			player = append(player, card)
-		}
+	ret.State = StatePlayerTurn
+	return ret
+}
+
+func Hit(gs GameState) GameState {
+	ret := clone(gs)
+	hand := ret.CurrentPlayer()
+	var card deck.Card
+	card, ret.Deck = draw(ret.Deck)
+	*hand = append(*hand, card)
+	if hand.Score() > 21 {
+		return Stand(gs)
 	}
-	// If dealer score <= 16, we hit
-	// If dealer has a soft 17, then we hit.
-	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
-		card, cards = draw(cards)
-		dealer = append(dealer, card)
-	}
-	pScore, dScore := player.Score(), dealer.Score()
+	return ret
+}
+
+func Stand(gs GameState) GameState {
+	ret := clone(gs)
+	ret.State++
+	return ret
+}
+
+func EndHand(gs GameState) GameState {
+	ret := clone(gs)
+	pScore, dScore := ret.Player.Score(), ret.Dealer.Score()
 	fmt.Println("==FINAL HANDS==")
-	fmt.Println("Player:", player, "\nScore:", pScore)
-	fmt.Println("Dealer:", dealer, "\nScore:", dScore)
+	fmt.Println("Player:", ret.Player, "\nScore:", pScore)
+	fmt.Println("Dealer:", ret.Dealer, "\nScore:", dScore)
 	switch {
 	case pScore > 21:
 		fmt.Println("You busted")
@@ -93,15 +107,88 @@ func main()  {
 	case dScore == pScore:
 		fmt.Println("Draw")
 	}
+	fmt.Println()
+	ret.Player = nil
+	ret.Dealer = nil
+	return ret
+}
 
-	//for i := 0; i < 10; i++ {
-	//	card, cards = cards[0], cards[1:]
-	//	fmt.Println(card)
-	//}
-	//var h Hand = cards[0:3]
-	//fmt.Println(h)
+func main()  {
+	var gs GameState
+	gs = Shuffle(gs)
+
+	for i := 0; i < 10; i++ {
+		gs = Deal(gs)
+
+		var input string
+		for gs.State == StatePlayerTurn {
+			fmt.Println("Player:", gs.Player)
+			fmt.Println("Dealer:", gs.Dealer.DealerString())
+			fmt.Println("What will you do? (h)it, (s)tand")
+			fmt.Scanf("%s\n", &input)
+			switch input {
+			case "h":
+				gs = Hit(gs)
+			case "s":
+				gs = Stand(gs)
+			default:
+				fmt.Println("Invalid option:", input)
+			}
+		}
+
+		for gs.State == StateDealerTurn {
+			// If dealer score <= 16, we hit
+			// If dealer has a soft 17, then we hit.
+			if gs.Dealer.Score() <= 16 || (gs.Dealer.Score() == 17 && gs.Dealer.MinScore() != 17) {
+				gs = Hit(gs)
+			} else {
+				gs = Stand(gs)
+			}
+		}
+
+		gs = EndHand(gs)
+	}
 }
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
 	return cards[0], cards[1:]
+}
+
+type State int8
+
+const (
+	StatePlayerTurn State = iota
+	StateDealerTurn
+	StateHandOver
+)
+
+type GameState struct {
+	Deck []deck.Card
+	State State
+	Player Hand
+	Dealer Hand
+}
+
+func (gs *GameState) CurrentPlayer() *Hand {
+	switch gs.State {
+	case StatePlayerTurn:
+		return &gs.Player
+	case StateDealerTurn:
+		return &gs.Dealer
+	default:
+		panic("Not currently any player's turn")
+	}
+}
+
+func clone(gs GameState) GameState {
+	ret := GameState{
+		Deck: make([]deck.Card, len(gs.Deck)),
+		State: gs.State,
+		Player: make([]deck.Card, len(gs.Player)),
+		Dealer: make([]deck.Card, len(gs.Dealer)),
+	}
+	copy(ret.Deck, gs.Deck)
+	copy(ret.Player, gs.Player)
+	copy(ret.Dealer, gs.Dealer)
+	return ret
 }
